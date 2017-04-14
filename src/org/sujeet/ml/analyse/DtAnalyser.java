2f9822caa365803e06f5f1c57b3f36e26fa2e8a0
SujeetS <SujeetS@nsdlegovnb43.nsdl.co.in>
2017-04-14 19:42:13 +0530

    INtegration Done Awaiting Testing

--- src/org/sujeet/ml/analyse/DtAnalyser.java
+++ src/org/sujeet/ml/analyse/DtAnalyser.java
@@ -4,10 +4,14 @@ import java.io.Serializable;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.classification.LogisticRegressionModel;
+import org.apache.spark.mllib.classification.SVMModel;
 import org.apache.spark.mllib.clustering.KMeansModel;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.mllib.tree.RandomForest;
 import org.apache.spark.mllib.tree.model.DecisionTreeModel;
+import org.apache.spark.mllib.tree.model.RandomForestModel;
 import org.sujeet.ml.BuildModel;
 //import org.apache.spark.mllib.linalg;
 
@@ -17,22 +21,40 @@ public class DtAnalyser /*implements Analyse*/ implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	static DecisionTreeModel model; 
+	static DecisionTreeModel DTmodel;
+	static LogisticRegressionModel LRmodel;
+	static RandomForestModel RFmodel;
+	static SVMModel SvmModel;
+	
+	
 	//@Override
 	public boolean processAndPredict(String input) {
-		// TODO Auto-generated method stub''
+		
 		String[] sarray = input.split(",");
 		double[] values = new double[sarray.length];
 		for (int i = 0; i < sarray.length; i++) {
 			values[i] = Double.parseDouble(sarray[i]);
 		}
 		Vector features=Vectors.dense(values);
-		Double prediction=model.predict(features);
-		//System.out.println(prediction);
-		return prediction==1.0?true:false;
+		
+		Double DTprediction=DTmodel.predict(features);
+		Double LRprediction=LRmodel.predict(features);
+		//Double SVMprediction=SvmModel.predict(features);
+		Double RFprediction=RFmodel.predict(features);
+		System.out.println("DT::"+DTprediction);
+		System.out.println("LR::"+LRprediction);
+		//System.out.println("SVM::"+SVMprediction);
+		System.out.println("RF::"+RFprediction);
+		System.out.println(""+(DTprediction+LRprediction+RFprediction));
+		
+		return (DTprediction+LRprediction+RFprediction)>=2.0?true:false;
+		
 	}
 
 	//@Override
+	
+
+	
 	public boolean report() {
 		// TODO Auto-generated method stub
 		return false;
@@ -40,8 +62,10 @@ public class DtAnalyser /*implements Analyse*/ implements Serializable{
 
 	public static boolean init(JavaSparkContext jsc) {
 		// TODO Auto-generated method stub
-		 model = DecisionTreeModel.load(jsc.sc(),
-				 BuildModel.PATH_FOR_SAVING_MODEL+"FullDataDT");
+		LRmodel	=LogisticRegressionModel.load(jsc.sc(),".\\resource\\models\\FullDataLR_13");
+		DTmodel	=DecisionTreeModel.load(jsc.sc(),".\\resource\\models\\FullDataDT_13");
+		RFmodel	=RandomForestModel.load(jsc.sc(),".\\resource\\models\\FullDataRF_13");
+		SvmModel=SVMModel.load(jsc.sc(),".\\resource\\models\\FullDataSVM_13");
 		return true;
 	}
 
@@ -65,8 +89,9 @@ public class DtAnalyser /*implements Analyse*/ implements Serializable{
 	    DtAnalyser dt=new DtAnalyser();
 	    String input="0.0,2.0,7.0,0.0,8.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,29.0,0.0,0.0,0.0,0.0,1.0,0.0,1.0,3.0,127.0,1.0,0.0,1.0,0.25,0.0,0.0,0.0,0.0,0.0,0.0";
 	    
-	    dt.init(jsc);
+	    DtAnalyser.init(jsc);
 	    dt.processAndPredict(input);
+	    System.out.println(dt.processAndPredict(input));
 	}
 	    
 }
